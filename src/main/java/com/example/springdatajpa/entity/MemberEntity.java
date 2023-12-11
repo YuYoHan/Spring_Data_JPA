e8afddd0288ca997797ce249b9ace9418fa61f2a
YuYoHan <zxzz8014@naver.com>
2023-12-11 19:21:11 +0900

    [Modify] 유저 엔티티 수정

--- src/main/java/com/example/springdatajpa/entity/MemberEntity.java
+++ src/main/java/com/example/springdatajpa/entity/MemberEntity.java
@@ -1,25 +1,36 @@
 package com.example.springdatajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@ToString(of = {"id", "userName", "age"})
 public class MemberEntity {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String userName;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private TeamEntity team;
 
     @Builder
-    public MemberEntity(Long id, String userName) {
+    public MemberEntity(Long id, String userName, int age, TeamEntity team) {
         this.id = id;
         this.userName = userName;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(TeamEntity team) {
+        this.team = team;
+        team.getMemberEntityList().add(this);
     }
 }
