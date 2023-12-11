18d9383990a9a3fcf7c6fed11db0fb3455d89138
YuYoHan <zxzz8014@naver.com>
2023-12-11 19:21:32 +0900

    [Modify] 유저 레포지토리 수정

--- src/main/java/com/example/springdatajpa/repository/MemberJpaRepository.java
+++ src/main/java/com/example/springdatajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext
@@ -15,7 +18,29 @@ public class MemberJpaRepository {
         return member;
     }
 
+    // 하나만 조회
     public MemberEntity find(Long id) {
         return em.find(MemberEntity.class, id);
+    }
+
+    public Optional<MemberEntity> findById(Long id) {
+        MemberEntity member = em.find(MemberEntity.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from MemberEntity m", Long.class)
+                .getSingleResult();
+    }
+
+    // 삭제
+    public void delete(MemberEntity member) {
+        em.remove(member);
+    }
+
+    // 전체 조회
+    public List<MemberEntity> findAll() {
+        return em.createQuery("select m from MemberEntity m", MemberEntity.class)
+                .getResultList();
     }
 }
