0ca8d929872f2cb832a6acb882fa6c1bcf6b453b
YuYoHan <zxzz8014@naver.com>
2023-12-15 15:23:17 +0900

    [Study] 조회시 memberDTO로 반환

--- src/main/java/com/example/springdatajpa/repository/MemberRepository.java
+++ src/main/java/com/example/springdatajpa/repository/MemberRepository.java
@@ -1,9 +1,32 @@
 package com.example.springdatajpa.repository;
 
+import com.example.springdatajpa.domain.MemberDTO;
 import com.example.springdatajpa.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    List<MemberEntity> findTop3HelloBy();
+
+    @Query(name = "Member.findByUserName")
+    List<MemberEntity> findByUserName(@Param("userName") String userName);
+
+    // JPQL의 장점은 오타가나면 애플리케이션 로딩시점에 오류가 발생한다.
+    @Query("select m from MemberEntity m where m.userName = :userName and m.age = :age")
+    List<MemberEntity> findUser(@Param("userName") String userName,
+                                @Param("age") int age);
+
+
+    @Query("select m.userName from MemberEntity m")
+    List<String> findUserNameList();
+
+    // DTO로 조회
+    @Query("select new com.example.springdatajpa.domain.MemberDTO(m.id, m.userName, t.name)" +
+            "from MemberEntity m join m.team t")
+    List<MemberDTO> findMemberDTO();
 }
--- src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,6 +23,7 @@ class MemberRepositoryTest {
     @Test
     @DisplayName("Spring data JPA 저장 테스트")
     void testMember() {
+        System.out.println("memberRepository : " + memberRepository.getClass());
         MemberEntity member = MemberEntity.builder()
                 .userName("memberA")
                 .build();
@@ -29,6 +33,42 @@ class MemberRepositoryTest {
                 .orElseThrow(EntityNotFoundException::new);
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-
     }
+
+    @Test
+    void basicCRUD() {
+        MemberEntity member1 = MemberEntity.builder()
+                .userName("member1")
+                .build();
+
+        MemberEntity member2 = MemberEntity.builder()
+                .userName("member2")
+                .build();
+
+        MemberEntity findMember1 = memberRepository.findById(member1.getId()).get();
+        MemberEntity findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1.getId()).isEqualTo(member1);
+        assertThat(findMember2.getId()).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<MemberEntity> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+    }
+
+    @Test
+    void findHelloBy() {
+        List<MemberEntity> helloBy = memberRepository.findTop3HelloBy();
+    }
+
+
+
 }
